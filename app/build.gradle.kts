602ddc7dc8398eef7e4d23c56c9d7279829575da
disesosoftware <pstlvictoras@yahoo.com>
2025-05-26 17:08:54 +0300

    added multiple FlavorTypes

---
+++ app/build.gradle.kts
@@ -0,0 +1,98 @@
+plugins {
+    id(BuildPlugins.ANDROID_APPLICATION)
+    id(BuildPlugins.KOTLIN_ANDROID)
+}
+
+android {
+    namespace = BuildConfig.APP_ID
+    compileSdk = BuildConfig.COMPILE_SDK_VERSION
+
+    defaultConfig {
+        applicationId = BuildConfig.APP_ID
+        minSdk = BuildConfig.MIN_SDK_VERSION
+        targetSdk = BuildConfig.TARGET_SDK_VERSION
+        versionCode = RealeaseConfig.VERSION_CODE
+        versionName = RealeaseConfig.VERSION_NAME
+
+        testInstrumentationRunner = TestBuildConfig.TEST_INSTRUMENTATION_RUNNER
+
+        ndk {
+            abiFilters += listOf("armeabi-v7a", "arm64-v8a", "x86", "x86_64")
+        }
+    }
+
+    buildTypes {
+       getByName(BuildTypes.RELEASE) {
+           proguardFiles(
+               getDefaultProguardFile("proguard-android-optimize.txt"),
+               "proguard-rules.pro"
+           )
+           isMinifyEnabled = Build.Release.isMinifyEnabled
+           enableUnitTestCoverage = Build.Release.enableUnitTestCoverage
+           isDebuggable = Build.Release.isDebuggable
+       }
+
+        getByName(BuildTypes.DEBUG){
+            applicationIdSuffix = Build.Debug.applicationIdSuffix
+            versionNameSuffix = Build.Debug.versionNameSuffix
+            isMinifyEnabled = Build.Debug.isMinifyEnabled
+            enableUnitTestCoverage = Build.Debug.enableUnitTestCoverage
+            isDebuggable = Build.Debug.isDebuggable
+        }
+
+        create(BuildTypes.RELEASE_EXTERNAL_QA) {
+            applicationIdSuffix = Build.ReleaseExternalQa.applicationIdSuffix
+            versionNameSuffix = Build.ReleaseExternalQa.versionNameSuffix
+            isMinifyEnabled = Build.ReleaseExternalQa.isMinifyEnabled
+            enableUnitTestCoverage = Build.ReleaseExternalQa.enableUnitTestCoverage
+        }
+    }
+
+    flavorDimensions.add(BuildDimensions.APP)
+    flavorDimensions.add(BuildDimensions.STORE)
+
+    productFlavors {
+        BuildFlavor.Google.create(this)
+        BuildFlavor.Huawei.create(this)
+        BuildFlavor.Driver.create(this)
+        BuildFlavor.Client.create(this)
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+    buildFeatures {
+        compose = true
+    }
+
+    composeOptions {
+        kotlinCompilerExtensionVersion = DependenciesVersions.COMPOSE_COMPILER
+    }
+
+}
+
+
+
+dependencies {
+    implementation(Dependencies.ANDROID_CORE)
+    implementation(Dependencies.ANDROID_LIFECYCLE_RUNTIME_KTX)
+    implementation(Dependencies.ANDROID_ACTIVITY_COMPOSE)
+    implementation(Dependencies.ANDROIDX_UI)
+    implementation(Dependencies.ANDROIDX_UI_GRAPHICS)
+    implementation(Dependencies.ANDROIDX_UI_TOOLING_PREVIEW)
+    implementation(Dependencies.ANDROIDX_MATERIAL3)
+
+    testImplementation(TestDependencies.ANDROIDX_JUNIT)
+
+    androidTestImplementation(TestDependencies.ANDROIDX_JUNIT)
+    androidTestImplementation(TestDependencies.ANDROIDX_ESPRESSO_CORE)
+    androidTestImplementation(platform(TestDependencies.ANDROIDX_COMPOSE_BOM))
+    androidTestImplementation(TestDependencies.ANDROIDX_COMPOSE_UI_TEST)
+
+    debugImplementation(Dependencies.ANDROIDX_UI_TOOLING_PREVIEW)
+    debugImplementation(TestDependencies.ANDROIDX_COMPOSE_UI_TEST_MANIFEST)
+}
